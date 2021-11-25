a8f87cc93b9bb405c79f2cd41afae54a056de99f
deang <dd0167@bs.amalnet.k12.il>
2021-11-25 20:57:12 +0200

    25/11/2021 - 21:00

--- app/src/main/java/com/example/betaversion/LoginActivity.java
+++ app/src/main/java/com/example/betaversion/LoginActivity.java
@@ -8,6 +8,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
@@ -17,11 +18,18 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
 public class LoginActivity extends AppCompatActivity {
 
     ProgressBar progressBar_login;
     EditText et_email_login;
     EditText et_password_login;
+    CheckBox checkBox_login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +39,7 @@ public class LoginActivity extends AppCompatActivity {
         progressBar_login=(ProgressBar) findViewById(R.id.progressBar_login);
         et_email_login=(EditText) findViewById(R.id.et_email_login);
         et_password_login=(EditText) findViewById(R.id.et_password_login);
+        checkBox_login=(CheckBox) findViewById(R.id.checkBox_login);
 
         progressBar_login.setVisibility(View.INVISIBLE);
 
@@ -64,6 +73,16 @@ public class LoginActivity extends AppCompatActivity {
                     progressBar_login.setVisibility(View.INVISIBLE);
                 }
             });
+
+            try {
+                FileOutputStream fos = openFileOutput("test.txt",MODE_PRIVATE);
+                OutputStreamWriter osw = new OutputStreamWriter(fos);
+                BufferedWriter bw = new BufferedWriter(osw);
+                bw.write(checkBox_login.isChecked()+"");
+                bw.close();
+            } catch (IOException e) {
+            }
+
             et_email_login.setText("");
             et_password_login.setText("");
         }
